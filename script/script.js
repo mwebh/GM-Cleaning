1a346614bed476d2c1599ed219a88cea9a2f5a1a
BitWebIT <106890622+BitWebIT@users.noreply.github.com>
2022-08-11 15:02:06 +0100

    -

--- script/script.js
+++ script/script.js
@@ -1,13 +1,34 @@
 const menuButton = document.querySelector(".menu");
 const mobileNav = document.querySelector(".mobile-nav");
 
+const barOne = document.querySelector(".bar1");
+const barTwo = document.querySelector(".bar2");
+const barThree = document.querySelector(".bar3");
+
 
 menuButton.addEventListener('click', () => {
     if (mobileNav.classList.contains("active")) {
         mobileNav.classList.add("hideNav");
         mobileNav.classList.remove("active");
+
+        barOne.classList.remove("bar1active");
+        barTwo.classList.remove("bar2active");
+        barThree.classList.remove("bar3active");
+
+        barOne.classList.add("returnbar1");
+        barTwo.classList.add("returnbar2");
+        barThree.classList.add("returnbar3");
+
     } else {
-        
+        barOne.classList.remove("returnbar1");
+        barTwo.classList.remove("returnbar2");
+        barThree.classList.remove("returnbar3");
+
+        barOne.classList.add("bar1active");
+        barTwo.classList.add("bar2active");
+        barThree.classList.add("bar3active");
+
+
         mobileNav.classList.add("active");
         mobileNav.classList.remove("hideNav");
     }
